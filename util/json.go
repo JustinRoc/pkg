57265432fd12c06e0ee8949c1a27cd6d3602cf63
hezhaopeng <2267805269@qq.com>
2025-08-10 17:27:15 +0800

    util

--- util/json.go
+++ util/json.go
@@ -4,11 +4,19 @@ import (
 	"encoding/json"
 )
 
-
-func JSONLog(v any) string {
+func ToJSONStr(v any) string {
 	b, err := json.Marshal(v)
 	if err != nil {
 		return err.Error()
 	}
 	return string(b)
+}
+
+func FromJSON[T any](data []byte) (T, error) {
+	var v T
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return v, err
+	}
+	return v, nil
 }
