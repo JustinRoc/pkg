2037e0f05b6477d9ca736f10ddbc8c5c576ef0ca
hezhaopeng <2267805269@qq.com>
2025-08-10 16:51:26 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JustinRoc/pkg
+
+go 1.22.5
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1
---
+++ slogw/logger.go
@@ -0,0 +1,148 @@
+package slogw
+
+import (
+	"context"
+	"fmt"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"log/slog"
+	"os"
+	"runtime"
+	"strings"
+)
+
+/* ------------------------------------------------  自定义handler  ------------------------------------------------ */
+
+const (
+	XRequestID = "X-Request-ID"
+)
+
+type CustomHandler struct {
+	handler slog.Handler
+}
+
+var contextHooks []func(ctx context.Context, record *slog.Record)
+
+func AddContextHook(hook func(ctx context.Context, record *slog.Record)) {
+	contextHooks = append(contextHooks, hook)
+}
+
+func (h *CustomHandler) Handle(ctx context.Context, record slog.Record) error {
+	if requestID, ok := ctx.Value(XRequestID).(string); ok {
+		record.AddAttrs(slog.String(XRequestID, requestID))
+	}
+	for _, hook := range contextHooks {
+		hook(ctx, &record)
+	}
+	return h.handler.Handle(ctx, record)
+}
+
+func (h *CustomHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.handler.Enabled(ctx, level)
+}
+
+func (h *CustomHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	newJsonHandler := h.handler.WithAttrs(attrs)
+	return &CustomHandler{handler: newJsonHandler}
+}
+
+func (h *CustomHandler) WithGroup(name string) slog.Handler {
+	return &CustomHandler{handler: h.handler.WithGroup(name)}
+}
+
+/* ------------------------------------------------  初始化  ------------------------------------------------ */
+
+var baseLogger *slog.Logger
+
+func Init(file string, level string, tags map[any]any) {
+	var output io.Writer
+	output = os.Stdout
+	if file != "" {
+		output = &lumberjack.Logger{
+			Filename:   file,
+			MaxSize:    20, // 以MB为单位
+			MaxBackups: 5,
+			MaxAge:     30,   // 以天为单位
+			Compress:   true, // 压缩旧日志文件
+		}
+	}
+
+	levelMap := map[string]slog.Leveler{
+		"debug": slog.LevelDebug,
+		"info":  slog.LevelInfo,
+		"warn":  slog.LevelWarn,
+		"error": slog.LevelError,
+	}
+
+	jsonHandler := slog.NewJSONHandler(output, &slog.HandlerOptions{
+		AddSource: false,
+		Level:     levelMap[level],
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				t := a.Value.Time()
+				_, offset := t.Zone()
+				return slog.String(a.Key, t.Format(fmt.Sprintf("2006-01-02 15:04:05.000 UTC+%d", offset/3600)))
+			}
+			return a
+		},
+	})
+
+	baseLogger = slog.New(&CustomHandler{jsonHandler})
+	for k, v := range tags {
+		baseLogger = baseLogger.With(k, v)
+	}
+}
+
+/* ------------------------------------------------  调用堆栈 ------------------------------------------------ */
+
+func caller() string {
+	_, file, line, _ := runtime.Caller(2)
+	pathSlice := strings.Split(file, "/")
+	if len(pathSlice) < 3 {
+		return fmt.Sprintf("%s:%d", file, line)
+	}
+	path := strings.Join(pathSlice[len(pathSlice)-2:], "/")
+	return fmt.Sprintf("%s:%d", path, line)
+}
+
+/* ------------------------------------------------  baseLogger ------------------------------------------------ */
+
+func DebugContext(ctx context.Context, msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.DebugContext(ctx, msg, args...)
+}
+
+func InfoContext(ctx context.Context, msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.InfoContext(ctx, msg, args...)
+}
+
+func WarnContext(ctx context.Context, msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.WarnContext(ctx, msg, args...)
+}
+
+func ErrorContext(ctx context.Context, msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.ErrorContext(ctx, msg, args...)
+}
+
+func Debug(msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.Debug(msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.Info(msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.Warn(msg, args...)
+}
+
+func Error(msg string, args ...any) {
+	args = append(args, "caller", caller())
+	baseLogger.Error(msg, args...)
+}
